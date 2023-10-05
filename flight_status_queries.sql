bfeb0d212392d9d0f90c238d84e11bf5e7aaecb8
ashley-ley <132225987+ashley-ley@users.noreply.github.com>
2023-10-05 17:40:12 -0500

    Add sql queries for flight_status
    Each country has their own query with flight status

---
+++ flight_status_queries.sql
@@ -0,0 +1,125 @@
+-- Create flight status queries based on country
+-- Create flight status queries for the month of March ("Spring Break")
+SELECT
+	count(flight_status),
+	flight_status,
+	'Mexico' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-03-01' AND '2022-03-31' and country_name = 'Mexico'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'Germany' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-03-01' AND '2022-03-31' and country_name = 'Germany'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'United Kingdom' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-03-01' AND '2022-03-31' and country_name = 'United Kingdom'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'Argentina' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-03-01' AND '2022-03-31' and country_name = 'Argentina'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'Italy' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-03-01' AND '2022-03-31' and country_name = 'Italy'
+GROUP BY flight_status
+
+
+-- Create flight status queries based on country
+-- Create flight status queries for the month of March ("Summer Break/Summer Vacation")
+SELECT
+	count(flight_status),
+	flight_status,
+	'Mexico' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-06-01' AND '2022-06-30' and country_name = 'Mexico'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'Germany' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-06-01' AND '2022-06-30' and country_name = 'Germany'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'United_Kingdom' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-06-01' AND '2022-06-30' and country_name = 'United_Kingdom'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'Argentina' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-06-01' AND '2022-06-30' and country_name = 'Argentina'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'Italy' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-06-01' AND '2022-06-30' and country_name = 'Italy'
+GROUP BY flight_status
+
+-- Create flight status queries based on country
+-- Create flight status queries for the month of March ("Holiday Season")
+SELECT
+	count(flight_status),
+	flight_status,
+	'Mexico' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-12-01' AND '2022-12-31' and country_name = 'Mexico'
+GROUP BY flight_status
+SELECT
+	count(flight_status),
+	flight_status,
+	'Germany' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-12-01' AND '2022-12-31' and country_name = 'Germany'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'United_Kingdom' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-12-01' AND '2022-12-31' and country_name = 'United Kingdom'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'Argentina' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-12-01' AND '2022-12-31' and country_name = 'Argentina'
+GROUP BY flight_status
+
+SELECT
+	count(flight_status),
+	flight_status,
+	'Italy' AS country
+FROM five_country_flights
+where departure_date BETWEEN '2022-12-01' AND '2022-12-31' and country_name = 'Italy'
+GROUP BY flight_status
